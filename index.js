83a45a051f4fc0e0cf6ab25eb4d97a500abdb401
Apelsin16 <apelsin@i.ua>
2018-08-28 15:16:01 +0300

    ready

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 const config = require("./config/development");
 const bodyParser = require('body-parser');
+const _ = require('lodash');
+const slug = require('slug');
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
@@ -30,7 +32,8 @@ const sendUsers = (req, res, next) => {
 
 const deleteUser = (req, res, next) => {
     const index = req.params.index;
-    USERS.filter((user, i) => i !== index )
+    req.users = USERS.filter((user, i) => i !== Number(index));
+    next();
 }
 
 const addUser = (req, res, next) => {
@@ -40,29 +43,81 @@ const addUser = (req, res, next) => {
     next();
 }
 
+const updateUser = (req, res, next) => {
+    const index = Number(req.params.index);
+    const updateData = req.body;
+    req.users = USERS.map(
+        (user, i ) => i === index
+        ? _.merge(user, updateData)
+        : user
+    );
+    next();
+}
+
+const updateBook = (req, res, next) => {
+    const index = Number(req.params.index);
+    const title = req.params.title;
+    const userBooks = USERS[index].books;
+    const updateData = req.body;
+    // const book = userBooks.find((bookEx => slug(bookEx.title).toLowerCase() === title.toLowerCase() ))
+    userBooks.filter( book =>
+        slug(book.title).toLowerCase() === title.toLowerCase()
+        ? _.merge(book, updateData)
+        : book
+    );
+    req.books = userBooks;
+    next();
+}
+
 const getBooks = (req, res, next) => {
     const index = req.params.index;
     req.books = USERS[index].books;
     next();
 };
+
+const addBook = (req, res, next) => {
+    const index = req.params.index;
+    const book = req.body;
+    req.books = USERS[index].books;
+    req.books.push(book);
+    next();
+}
+
 const sendBooks = (req, res, next) => {
     res.status(200);
     res.json(req.books);
+    next();
+}
+
+const deleteBook  = (req, res, next) => {
+    const index = Number(req.params.index);
+    const title = req.params.title;
+    req.books = USERS[index].books;
+    req.books = req.books.filter(
+        book => slug(book.title) === title
+    )
+}
+
+const getBooksByTitle = (req, res, next) => {
+    const { title, index } = req.params;
+    const userBooks = USERS[index].books;
+    req.books = userBooks.find( book => slug(book.title) === title );
+    next();
 }
 
 // Users
 app.get("/users/", getUsers, sendUsers);
 app.post("/users/", addUser, sendUsers);
-app.delete("/users/:index/");
-app.put("/users/:index/"); // (Lodash) _.merge
+app.delete("/users/:index/", deleteUser, sendUsers);
+app.put("/users/:index/", updateUser, sendUsers); // (Lodash) _.merge
 
 // Books
 app.get("/users/:index/books", getBooks, sendBooks);
-app.post("/users/:index/books");
-app.put("/users/:index/books/:title");
-app.delete("/users/:index/books/:title");
+app.post("/users/:index/books", addBook, sendBooks);
+app.put("/users/:index/books/:title", updateBook, sendBooks);
+app.delete("/users/:index/books/:title", deleteBook, sendBooks);
 // *
-app.get("/users/:index/books/:title");
+app.get("/users/:index/books/:title", getBooksByTitle, sendBooks);
 
 // app.post("/users/")
 // app.put("/users/")
